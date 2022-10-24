5ba33f5d93eafb7451f92dd8810ded6394264e3d
xuantien <taxuantien312000@gmail.com>
2022-10-24 17:31:37 +0700

    add file

--- src/chat/chatContent/input.js
+++ src/chat/chatContent/input.js
@@ -1,6 +1,12 @@
-import { FileImageOutlined } from "@ant-design/icons";
-import { Button, Input, Upload } from "antd";
-import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
+import { CloseCircleOutlined, FileImageOutlined } from "@ant-design/icons";
+import { Button, Input } from "antd";
+import {
+  deleteObject,
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
 import React, { useContext, useState } from "react";
 import styled from "styled-components";
 import { storage } from "../../firebase/config";
@@ -9,13 +15,24 @@ import { AuthContext } from "./../../context/authContext";
 import { addDocument } from "./../../firebase/service";
 const FormStyle = styled.div`
   display: flex;
+  flex-direction: column;
 `;
 const ButtonStyle = styled(Button)`
   background-color: #6a7cc5;
 `;
 const InputStyle = styled(Input)`
   padding: 5px;
+  display: flex;
 `;
+const IconXStyle = styled(CloseCircleOutlined)`
+  position: absolute;
+  top: -30px;
+  right: -4px;
+  color: white;
+  background-color: black;
+  border-radius: 100%;
+`;
+
 function InputText(props) {
   const [message, setMessage] = useState("");
   const user = useContext(AuthContext);
@@ -32,49 +49,112 @@ function InputText(props) {
     });
     setMessage("");
   };
-  const uploadFile = (e) => {
-    console.log(e);
-    const storageRef = ref(storage, `files/${e.file.name}`);
-    const metadata = {
-      contentType: e.file.type,
-    };
-    const uploadTask = uploadBytesResumable(storageRef, e.file, metadata);
+
+  const handleSubmit = (_file) => {
+    const file = _file;
+    if (!file) return;
+
+    const storageRef = ref(storage, `files/${file.name}`);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
     uploadTask.on(
       "state_changed",
       (snapshot) => {
         const progress = Math.round(
           (snapshot.bytesTransferred / snapshot.totalBytes) * 100
         );
-        console.log(progress);
+        // setProgresspercent(progress);
       },
       (error) => {
         alert(error);
       },
       () => {
         getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setImgs((prev) => [
+            ...prev,
+            { downloadURL, fileName: file.name, type: file.type },
+          ]);
           console.log(downloadURL);
-          setImgs([...imgs, downloadURL]);
         });
       }
     );
   };
+  const saveFile = (e) => {
+    Promise.all(
+      Object.values(e.target.files).map((file) => handleSubmit(file))
+    ).then((res) => console.log(res));
+  };
+  const deleteImg = (file, idex) => {
+    const storage = getStorage();
+
+    // Create a reference to the file to delete
+    const desertRef = ref(storage, `files/${file.fileName}`);
+
+    // Delete the file
+    deleteObject(desertRef)
+      .then(() => {
+        // File deleted successfully
+        setImgs(imgs.filter((i, index) => index !== idex));
+      })
+      .catch((error) => {
+        // Uh-oh, an error occurred!
+      });
+  };
   return (
-    <FormStyle>
-      <img src={[imgs[0]]} />
-      <InputStyle
-        suffix={
-          <Upload onChange={(e) => uploadFile(e)}>
-            <FileImageOutlined style={{ cursor: "pointer" }} />
-          </Upload>
-        }
-        value={message}
-        placeholder="Nhập nội dung"
-        onChange={(e) => setMessage(e.target.value)}
-      />
-      <ButtonStyle type="primary" onClick={addMessage}>
-        Gửi
-      </ButtonStyle>
-    </FormStyle>
+    <>
+      <FormStyle>
+        {imgs?.length > 0 && (
+          <div
+            style={{
+              border: "1px solid",
+              padding: "15px 5px",
+              borderRadius: "5px",
+              backgroundColor: "gray",
+            }}
+          >
+            {imgs.map((img, index) => (
+              <span style={{ position: "relative" }}>
+                <img
+                  style={{
+                    width: "70px",
+                    height: "70px",
+                    margin: "0 5px",
+                    borderRadius: "5px",
+                  }}
+                  src={img.downloadURL}
+                />
+                <IconXStyle onClick={() => deleteImg(img, index)} />
+              </span>
+            ))}
+          </div>
+        )}
+
+        <div>
+          <InputStyle
+            suffix={
+              <>
+                <input
+                  style={{ display: "none" }}
+                  id="file"
+                  type={"file"}
+                  multiple
+                  onChange={(e) => saveFile(e)}
+                />
+                <label htmlFor="file">
+                  <FileImageOutlined style={{ cursor: "pointer" }} />
+                </label>
+                <ButtonStyle type="primary" onClick={addMessage}>
+                  Gửi
+                </ButtonStyle>
+              </>
+            }
+            value={message}
+            placeholder="Nhập nội dung"
+            onChange={(e) => setMessage(e.target.value)}
+          />
+        </div>
+      </FormStyle>
+    </>
   );
 }
 
